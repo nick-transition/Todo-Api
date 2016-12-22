e764a7ff36e8f0aab8fbc4d1afbabc000b06bc0d
nick-transition <nstoddar@gmail.com>
2016-12-22 11:15:50 -0600

    Added description query param

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ app.get('/todos',function(req,res){
     filteredTodos = _.where(filteredTodos, {completed: false});
   }
 
+  if (queryParams.hasOwnProperty('q') && queryParams.q.length>0) {
+    filteredTodos = _.filter(filteredTodos,function (elem) {
+      return elem.description.toLowerCase().indexOf(queryParams.q) > -1;
+    });
+  }
+
   res.json(filteredTodos);
 });
 
